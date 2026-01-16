cd3df16238717de4fda8ce45e72a9fc7fbae803f
TurzynskiMaks <maksturzynski2137@gmail.com>
2026-01-16 18:17:54 +0100

    Removed @ResponseStatus from FilmNotFoundException.java, as our global handler does it anyway.

--- src/main/java/pl/maksturzynski/cinemabooking/exception/FilmNotFoundException.java
+++ src/main/java/pl/maksturzynski/cinemabooking/exception/FilmNotFoundException.java
@@ -1,10 +1,5 @@
 package pl.maksturzynski.cinemabooking.exception;
 
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class FilmNotFoundException extends RuntimeException {
     public FilmNotFoundException(Long id) {
         super("Film not found: " + id);
