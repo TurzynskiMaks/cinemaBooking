e019ccdda6ccb66f57a57b9ec07017bcbb9fb0b7
TurzynskiMaks <maksturzynski2137@gmail.com>
2026-01-17 01:58:05 +0100

    Added SeatStatus.java, ENUM for seat status, Added SeatReservation.java entity

---
+++ src/main/java/pl/maksturzynski/cinemabooking/domain/entity/SeatReservation.java
@@ -0,0 +1,42 @@
+package pl.maksturzynski.cinemabooking.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "seat_reservation",
+uniqueConstraints = @UniqueConstraint(name = "uq_sr", columnNames = {"screening_id", "seat_id"}))
+public class SeatReservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "screening_id", nullable = false)
+    @Getter
+    @Setter
+    private Screening screening;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "seat_id", nullable = false)
+    @Getter
+    @Setter
+    private Seat seat;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    @Getter
+    @Setter
+    private SeatStatus status;
+
+    @Column(name = "held_until")
+    @Getter
+    @Setter
+    private LocalDateTime heldUntil;
+}
