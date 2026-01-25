d7c47678dc752fd7c93e2c0bfe9013f749ed5310
TurzynskiMaks <maksturzynski2137@gmail.com>
2026-01-26 00:21:11 +0100

    Added scheduled job for cleaning purposes: every 60 seconds we clean up all held seat statutes that are expired. Furthermore, even if that doesn't work for some reason, we still show them as available.

--- src/main/java/pl/maksturzynski/cinemabooking/CinemaBookingApplication.java
+++ src/main/java/pl/maksturzynski/cinemabooking/CinemaBookingApplication.java
@@ -3,7 +3,9 @@ package pl.maksturzynski.cinemabooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties(pl.maksturzynski.cinemabooking.config.MailProperties.class)
 public class CinemaBookingApplication {
