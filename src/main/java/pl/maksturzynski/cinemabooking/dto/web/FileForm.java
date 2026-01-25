8553d46a5fde76d7344e71dcf27e5395b8d6e59b
TurzynskiMaks <maksturzynski2137@gmail.com>
2026-01-26 00:27:48 +0100

    Added FileForm.java (dto) for input validation.

---
+++ src/main/java/pl/maksturzynski/cinemabooking/dto/web/FileForm.java
@@ -0,0 +1,18 @@
+package pl.maksturzynski.cinemabooking.dto.web;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FileForm {
+    @NotBlank @Size(max = 200) private String title;
+    @NotBlank @Size(max = 100) private String genre;
+    @NotNull private Integer ageRating;
+    @NotBlank @Size(max = 150) private String director;
+    @NotBlank @Size(max = 500) private String castText;
+    @Size(max = 500) private String trailerUrl;
+}
