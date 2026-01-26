d53f3f01feec61cbc1cf3472d838a74d98aabb49
TurzynskiMaks <maksturzynski2137@gmail.com>
2026-01-26 12:18:40 +0100

    Added new data to .env-example Rebuilt Dockerfile so it works now Created docker-compose.yml for simplified app running Changed SecurityConfig.java to block /admin/** sites Changed application.yml and application-docker.yml to include admin credentials

--- src/main/java/pl/maksturzynski/cinemabooking/config/SecurityConfig.java
+++ src/main/java/pl/maksturzynski/cinemabooking/config/SecurityConfig.java
@@ -3,24 +3,21 @@ package pl.maksturzynski.cinemabooking.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/swagger-ui.html", "/swagger-ui/**",
-                                "/api-docs/**",
-                                "/favicon.ico", "h2-console/**"
-                        ).permitAll()
-
+                        .requestMatchers("/admin/**", "/admin").authenticated()
+                        .anyRequest().permitAll()
                 )
-                .csrf(csrf -> csrf.disable()) // albo csrf.disable() w dev
-                .headers(h -> h.frameOptions(f -> f.sameOrigin()))
+                .httpBasic(b -> {})
                 .build();
     }
 }
