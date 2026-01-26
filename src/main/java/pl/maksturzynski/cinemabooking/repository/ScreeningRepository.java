64a3a7e3e4ab42bf24fa370245a44192e250b74d
TurzynskiMaks <maksturzynski2137@gmail.com>
2026-01-26 12:39:21 +0100

    Repo preparations for removing films with past screenings.

--- src/main/java/pl/maksturzynski/cinemabooking/repository/ScreeningRepository.java
+++ src/main/java/pl/maksturzynski/cinemabooking/repository/ScreeningRepository.java
@@ -11,4 +11,7 @@ public interface ScreeningRepository extends JpaRepository<Screening, Long> {
     List<Screening> findByStartTimeBetween (LocalDateTime from, LocalDateTime to);
     List<Screening> findByHallIdAndStartTimeBetween(Long hallId, LocalDateTime from, LocalDateTime to);
     boolean existsByFilmId(Long filmId);
+    boolean existsByFilmIdAndStartTimeGreaterThanEqual(Long filmId, LocalDateTime from);
+    List<Screening> findAllByFilmId(Long filmId);
+    void deleteAllByFilmId(Long filmId);
 }
--- src/main/java/pl/maksturzynski/cinemabooking/repository/TicketRepository.java
+++ src/main/java/pl/maksturzynski/cinemabooking/repository/TicketRepository.java
@@ -3,5 +3,8 @@ package pl.maksturzynski.cinemabooking.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.maksturzynski.cinemabooking.domain.entity.Ticket;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    void deleteByScreeningIdIn(List<Long> screeningIds);
 }
